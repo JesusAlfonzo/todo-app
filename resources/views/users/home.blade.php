03cc538be943f8e5f5140c8d3c7fbd487f0935b2
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-03-29 17:22:50 -0400

    View: Dashboard con tareas de los usuarios.

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -5,8 +5,46 @@
 @auth
     <h1>Hola bienvenido</h1>
 
+    <button><a href="{{ route('profile.index') }}">Perfil del usuario</a></button>
+
+
     <form action="{{ route('login.destroy') }}" method="POST">
         @csrf
         <button type="submit">Cerrar Sesion</button>
     </form>
+
+    <button><a href="{{ route('task.create') }}">Crear una tarea</a></button>
+
+    <br>
+
+    <table>
+        <tr>
+            <th>Tarea</th>
+            <th>Contenido</th>
+            <th>Acciones</th>
+        </tr>
+        @foreach ($Tasks as $task)
+            <tr>
+
+                <td>{{ $task->title }}</td>
+                <td>{{ $task->content }}</td>
+            </tr>
+            <tr>
+                <td>
+                    <form action="{{ route('task.destroy', $task) }} method="post">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit">eliminar nota</button>
+                    </form>
+
+                    <button><a href="/task/{{ $task->id }}/edit">Editar Tarea</a></button>
+                    <button><a href="/task/{{ $task->id }}/show">Ver Tarea</a></button>
+                 
+
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+    <br>
 @endauth
