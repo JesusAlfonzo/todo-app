da304631901236d118934b71f91dc19e428a60e6
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-03-29 17:14:55 -0400

    Rutas: Se crearon las rutas CRUD para Profiles y tasks

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,57 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Tasks\TaskController;
 use App\Http\Controllers\Users\DashboardController;
+use App\Http\Controllers\Users\UserController;
 use Illuminate\Support\Facades\Route;
 
+// Pagina "Raiz"
 Route::get('/', function () {
     return view('welcome');
 });
 
-// --- RUTAS DE REGISTRO ---
-// Muestra el formulario
-Route::get('/register', [RegisterController::class, 'create'])->name('register.index'); 
-// Procesa el formulario
+// Rutas para "Registro" de usuarios
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
 Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
-// --- RUTAS DE LOGIN Y LOGOUT ---
-// Muestra el formulario de login (asumiendo que tu método se llama create o index)
-Route::get('/login', [LoginController::class, 'create'])->name('login.index'); 
-// Procesa el inicio de sesión
-Route::post('/login', [LoginController::class, 'store'])->name('login.store'); 
-// Procesa el cierre de sesión (Fíjate que ya no pide ID y usa POST)
-Route::post('/logout', [LoginController::class, 'destroy'])->name('login.destroy');
+// Rutas para "Login" de usuarios
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
 
-// --- RUTAS PROTEGIDAS ---
-// Para el dashboard suele ser mejor una ruta sencilla a menos que realmente hagas un CRUD completo ahí
-Route::get('/home', [DashboardController::class, 'index'])->name('home.index');
+// Rutas Autenticadas para la aplicacion
+Route::middleware('auth')->group(function () {
+
+    // Dashboard del usuario
+    Route::get('/home', [DashboardController::class, 'index'])->name('home.index');
+
+    // Logout del usuario
+    Route::post('/logout', [LoginController::class, 'destroy'])->name('login.destroy');
+
+    // Tareas del usuario
+    //Pagina index de tareas
+    Route::get('/task', [TaskController::class, 'index'])->name('task.index');
+
+    //Pagina formulario de creacion de una tarea
+    Route::get('/task/create', [TaskController::class, 'create'])->name('task.create');
+
+    Route::get('/task/{id}/edit', [TaskController::class, 'edit'])->name('task.edit');
+
+    Route::get('/task/{id}/show', [TaskController::class, 'show'])->name('task.show');
+
+    Route::put('/task/{id}', [TaskController::class, 'update'])->name('task.update');
+
+    Route::get('/task/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
+
+    //Funcion store para almacenar la data de la creacion de una tarea (para registrarla en la base de datos)
+    Route::post('/task', [TaskController::class, 'store'])->name('task.store'); //Store para datos de la tarea
+
+    Route::get('/profile', [UserController::class, 'index'])->name('profile.index');
+
+    Route::get('/profile/{id}/edit', [UserController::class, 'edit'])->name('profile.edit');
+    
+    Route::put('/profile/{id}', [UserController::class, 'update'])->name('profile.update');
+
+    Route::get('/profile/{id}', [UserController::class, 'destroy'])->name('profile.destroy');
+
+});
