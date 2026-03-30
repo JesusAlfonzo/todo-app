35f203019ee6996fdb4562beacbff1ad129d1133
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-03-29 22:41:11 -0400

    Rutas: Refactorizacion y Comentarios para mejor identificacion.

--- routes/web.php
+++ routes/web.php
@@ -7,52 +7,41 @@ use App\Http\Controllers\Users\DashboardController;
 use App\Http\Controllers\Users\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Pagina "Raiz"
+// -- LANDING PAGE --
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Rutas para "Registro" de usuarios
-Route::get('/register', [RegisterController::class, 'create'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+// -- REGISTER --
+Route::get('/register', [RegisterController::class, 'create'])->name('register'); // -> Register Form View
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store'); // -> Register User
 
-// Rutas para "Login" de usuarios
-Route::get('/login', [LoginController::class, 'create'])->name('login');
-Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+// -- LOGIN --
+Route::get('/login', [LoginController::class, 'create'])->name('login'); // -> Login Form View
+Route::post('/login', [LoginController::class, 'store'])->name('login.store'); // -> Login User
 
-// Rutas Autenticadas para la aplicacion
+// -- Flujio App (Auth Routes) --
 Route::middleware('auth')->group(function () {
 
-    // Dashboard del usuario
-    Route::get('/home', [DashboardController::class, 'index'])->name('home.index');
+    // -- Dashboard --
+    Route::get('/home', [DashboardController::class, 'index'])->name('home.index'); // -> Dashboard User
 
-    // Logout del usuario
-    Route::post('/logout', [LoginController::class, 'destroy'])->name('login.destroy');
+    // -- Logout --
+    Route::post('/logout', [LoginController::class, 'destroy'])->name('login.destroy'); // -> Logout - Delete Session
 
-    // Tareas del usuario
-    //Pagina index de tareas
-    Route::get('/task', [TaskController::class, 'index'])->name('task.index');
+    // -- TASKS --
+    // Route::get('/task', [TaskController::class, 'index'])->name('task.index'); // -> Tasks Index (No using)
+    Route::get('/task/create', [TaskController::class, 'create'])->name('task.create'); // -> Create Task Form view
+    Route::post('/task', [TaskController::class, 'store'])->name('task.store'); // -> Store Task
+    Route::get('/task/{id}/edit', [TaskController::class, 'edit'])->name('task.edit'); // -> Edit Task
+    Route::put('/task/{id}', [TaskController::class, 'update'])->name('task.update'); // -> Update Task
+    Route::get('/task/{id}/show', [TaskController::class, 'show'])->name('task.show'); // -> Show Task
+    Route::get('/task/{id}', [TaskController::class, 'destroy'])->name('task.destroy'); // -> Destroy - Delete Task
 
-    //Pagina formulario de creacion de una tarea
-    Route::get('/task/create', [TaskController::class, 'create'])->name('task.create');
+    // -- PROFILE (User Info) --
 
-    Route::get('/task/{id}/edit', [TaskController::class, 'edit'])->name('task.edit');
-
-    Route::get('/task/{id}/show', [TaskController::class, 'show'])->name('task.show');
-
-    Route::put('/task/{id}', [TaskController::class, 'update'])->name('task.update');
-
-    Route::get('/task/{id}', [TaskController::class, 'destroy'])->name('task.destroy');
-
-    //Funcion store para almacenar la data de la creacion de una tarea (para registrarla en la base de datos)
-    Route::post('/task', [TaskController::class, 'store'])->name('task.store'); //Store para datos de la tarea
-
-    Route::get('/profile', [UserController::class, 'index'])->name('profile.index');
-
-    Route::get('/profile/{id}/edit', [UserController::class, 'edit'])->name('profile.edit');
-    
-    Route::put('/profile/{id}', [UserController::class, 'update'])->name('profile.update');
-
-    Route::get('/profile/{id}', [UserController::class, 'destroy'])->name('profile.destroy');
-
+    Route::get('/profile', [UserController::class, 'index'])->name('profile.index'); // -> Profile Index 
+    Route::get('/profile/{id}/edit', [UserController::class, 'edit'])->name('profile.edit'); // -> Edit Profile
+    Route::put('/profile/{id}', [UserController::class, 'update'])->name('profile.update'); // -> Update Profile
+    Route::get('/profile/{id}', [UserController::class, 'destroy'])->name('profile.destroy'); // -> Destroy - Delete User
 });
